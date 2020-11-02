a6c69439bad0e249410fbaf415669ef1df886144
Gulmohar <gulmoharsontakke@gmail.com>
2020-11-02 10:03:16 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const keys = require('./keys');
+
+// Express App Setup
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = 8080;
+const headerRequestIdTokenName = 'x-custom-client-nginx-request-id';
+
+// Express route handlers
+
+const basePath = '/api/v1/backend-service';
+// const basePath = '';
+
+app.get(basePath + '/', (req, res) => {
+  res.send('Hi');
+});
+app.get(basePath + '/health', (req, res) => {
+  res.send('I am healthy!!!!!!!!!!!!!!!');
+});
+app.get(basePath + '/value/:id', (req, res) => {
+  const { id } = req.params;
+  res.send('return - ' + id);
+});
+
+app.get(basePath + '/getSquare/:id', (req, res) => {
+  const { id } = req.params;
+
+  // console.log(`
+
+  // requestHeader : ${JSON.stringify(req.headers)}
+
+  // `);
+  // console.log(`
+
+  // requestHeader in rrrrrr : ${req.headers[headerRequestIdTokenName]}
+
+  // `);
+
+  const output = {};
+  output[id] = id * id;
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('x-response-header-square', req.headers[headerRequestIdTokenName]);
+  res.send(output);
+});
+
+app.get(basePath + '/getCube/:id', (req, res) => {
+  const { id } = req.params;
+
+  const output = {};
+  output[id] = id * id * id;
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('x-response-header-cube', req.headers[headerRequestIdTokenName]);
+  res.send(output);
+});
+
+app.post(basePath + '/values', async (req, res) => {
+  const index = req.body.index;
+
+  console.log(`POST API INPUT -  : ${index}`);
+
+  if (parseInt(index) > 40) {
+    return res.status(422).send('Index too high');
+  }
+
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('x-response-header-values', req.headers[headerRequestIdTokenName]);
+  res.send({ index: index });
+});
+
+app.listen(PORT, () => console.log('App listening on port ' + PORT));
---
+++ tests/index.test.js
@@ -0,0 +1,8 @@
+describe('My Test Suite', () => {
+  it('My Success Test Case', () => {
+    expect(true).toEqual(true);
+  });
+  it('My Failure Test Case', () => {
+    expect(false).toEqual(false);
+  });
+});
